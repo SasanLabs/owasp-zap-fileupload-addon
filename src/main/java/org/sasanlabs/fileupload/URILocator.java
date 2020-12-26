c1de48eec0ce37f2145ade486a9dc33636898f72
karan preet singh sasan <karanpreet_singh@intuit.com>
2020-12-26 14:14:47 +0530

    SmallFix

--- src/main/java/org/sasanlabs/fileupload/URILocator.java
+++ src/main/java/org/sasanlabs/fileupload/URILocator.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright 2020 SasanLabs
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.sasanlabs.fileupload;
 
 import org.apache.commons.httpclient.URI;
@@ -5,8 +18,8 @@ import org.apache.commons.httpclient.URIException;
 import org.parosproxy.paros.network.HttpMessage;
 
 /**
- * {@code URILocator} class is used to find the URL either by parsing the {@code
- * HttpMessage} or reading the configuration mentioned in the options tab.
+ * {@code URILocator} class is used to find the URL either by parsing the {@code HttpMessage} or
+ * reading the configuration mentioned in the options tab.
  *
  * <p>This class also handles the "regex based configuration" e.g "url/$fileName"
  *
