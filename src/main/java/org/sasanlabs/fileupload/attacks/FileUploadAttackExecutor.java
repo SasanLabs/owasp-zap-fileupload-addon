664c534271973649cd77a4a2acc46dc58764d8ec
unknown <tomerpacific@gmail.com>
2023-10-15 19:35:12 +0300

    feature/stop-addon-vulnerability logic to check condition if should continue sending requests

--- src/main/java/org/sasanlabs/fileupload/attacks/FileUploadAttackExecutor.java
+++ src/main/java/org/sasanlabs/fileupload/attacks/FileUploadAttackExecutor.java
@@ -29,6 +29,7 @@ import org.sasanlabs.fileupload.attacks.rce.php.ImageWithPHPSnippetFileUpload;
 import org.sasanlabs.fileupload.attacks.rce.php.SimplePHPFileUpload;
 import org.sasanlabs.fileupload.attacks.xss.HtmlFileUpload;
 import org.sasanlabs.fileupload.attacks.xss.SVGFileUpload;
+import org.sasanlabs.fileupload.configuration.FileUploadConfiguration;
 import org.sasanlabs.fileupload.exception.FileUploadException;
 
 /**
@@ -70,11 +71,17 @@ public class FileUploadAttackExecutor {
     }
 
     public boolean executeAttack() throws FileUploadException {
+
+        Boolean shouldSendRequestsAfterFindingVulnerability = FileUploadConfiguration
+                .getInstance()
+                .getSendRequestsAfterFindingVulnerability();
+
         for (AttackVector attackVector : attackVectors) {
             if (this.fileUploadScanRule.isStop()) {
                 return false;
             } else {
-                if (attackVector.execute(this)) {
+                if (attackVector.execute(this) &&
+                        !shouldSendRequestsAfterFindingVulnerability) {
                     return true;
                 }
             }
